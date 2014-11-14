7d8599792af5fa1d2b1ecff04aae5034e2d5123d
Nikolay Mihaylov <nmmm@nmmm.nu>
2014-11-15 00:48:23 +0200

    fix typo in test_hm.c

--- test_hm.c
+++ test_hm.c
@@ -61,13 +61,13 @@ void hm_list_test(){
 	hm_list_put(bucket, hm_pair_create("lang",	"C/C++",	0));
 	hm_list_put(bucket, hm_pair_create("first",	"list top",	0));
 
-	PRINTF_TEST("HMBucket", "count",	hm_list_count(bucket) == 3				);
-	PRINTF_TEST("HMBucket", "exists",	hm_list_exists(bucket, "name")				);
-	PRINTF_TEST("HMBucket", "! exists",	! hm_list_exists(bucket, "computer")			);
+	PRINTF_TEST("HM List", "count",		hm_list_count(bucket) == 4		);
+	PRINTF_TEST("HM List", "exists",	hm_list_exists(bucket, "name")		);
+	PRINTF_TEST("HM List", "! exists",	! hm_list_exists(bucket, "computer")	);
 
-	PRINTF_TEST("HMBucket", "! get",	hm_list_get(bucket, "computer") == NULL			);
+	PRINTF_TEST("HM List", "! get",	hm_list_get(bucket, "computer") == NULL		);
 	const HMPair *pair = hm_list_get(bucket, "name");
-	PRINTF_TEST("HMBucket", "get",		hm_pair_equals(pair, "name")				);
+	PRINTF_TEST("HM List", "get",		hm_pair_equals(pair, "name")		);
 
 	hm_list_print(bucket);
 
@@ -76,21 +76,21 @@ void hm_list_test(){
 	hm_list_remove(bucket, "first"); // remove first element of the linked list
 	hm_list_remove(bucket, "lang");
 
-	PRINTF_TEST("HMBucket", "count",	hm_list_count(bucket) == 2				);
+	PRINTF_TEST("HM List", "count",	hm_list_count(bucket) == 2			);
 
 	hm_list_remove(bucket, "name");
 	hm_list_remove(bucket, "age");
 
-	PRINTF_TEST("HMBucket", "count",	hm_list_count(bucket) == 0				);
+	PRINTF_TEST("HM List", "count",	hm_list_count(bucket) == 0			);
 
-	PRINTF_TEST("HMBucket", "free",		hm_list_free(bucket) 					);
+	PRINTF_TEST("HM List", "free",		hm_list_free(bucket) 			);
 
 	// add some more pairs to test hm_list_free() with data
 	hm_list_put(bucket, hm_pair_create("name",	"niki",		0));
 	hm_list_put(bucket, hm_pair_create("age",	"5",		0));
 	hm_list_put(bucket, hm_pair_create("lang",	"C/C++",	0));
 
-	PRINTF_TEST("HMBucket", "free",		hm_list_free(bucket)					);
+	PRINTF_TEST("HM List", "free",		hm_list_free(bucket)			);
 }
 
 
@@ -143,9 +143,9 @@ void hm_print_sizes(){
 
 
 int main(int argc, char **argv){
-//	hm_pair_test(0);
+	hm_pair_test(0);
 
-//	hm_list_test();
+	hm_list_test();
 
 	hm_table_test();
 
